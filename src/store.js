890322cf4d1a366335ada67f7d8e0f3b4d7cdbd7
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-09 15:15:13 -0600

    install and import redux thunk

--- src/store.js
+++ src/store.js
@@ -1,2 +1,2 @@
-//import a few different libraries from redux: createStore, applyMiddleware, combineReducers
-//import thunk from redux-thunk(make asynchronous calls)
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux';
