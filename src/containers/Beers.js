b453d208f04b151f6d7027f94abad0bda5d81fb9
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-08 20:35:59 -0600

    fix warning of unique keys

--- src/containers/Beers.js
+++ src/containers/Beers.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Beers = (props) => {
   return (
     <div>
-      {props.beers.map(beer => <h1>{beer.name}</h1>)}
+      {props.beers.map(
+        beer => <h1 key={beer.id}>{beer.name}</h1>
+      )}
     </div>
   )
 }
