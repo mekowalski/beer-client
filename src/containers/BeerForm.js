5aedf7fb04bd52b001985d62b7bceb9ef36bb29e
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-09 12:04:16 -0600

    create and add BeerForm details

---
+++ src/containers/BeerForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class BeerForm extends React.Component{
+  render() {
+    return (
+      <div>
+        Fill the form out
+      </div>
+    )
+  }
+}
+export default BeerForm
