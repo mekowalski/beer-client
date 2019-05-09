117d5d154ece48ebc0b73a329059fe39dbb651a8
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-09 11:58:58 -0600

    refactor to class component

--- src/containers/Beers.js
+++ src/containers/Beers.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import BeerDetail from '../components/BeerDetail';
 
-const Beers = (props) => {
-  return (
-    <div>
-      <h1>Beer List</h1>
-      {props.beers.map(beer => <BeerDetail key={beer.id} beer={beer} />)}
-    </div>
-  )
+class Beers extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Beer List</h1>
+        {this.props.beers.map(beer => <BeerDetail key={beer.id} beer={beer} />)}
+      </div>
+    )
+  }
 }
 
 export default Beers
