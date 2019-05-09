7c7a3591195dd8628d132666075fdb08716a96e0
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-09 15:19:06 -0600

    create reducer with switch statement

--- src/store.js
+++ src/store.js
@@ -1,2 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux';
+
+//create reducer for beers
+const beers = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_BEERS':
+      return action.beer
+    default:
+      return state
+  }
+}
