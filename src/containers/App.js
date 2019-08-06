26edffd1af0dc447ba963f61f230234b89efb6bc
Malind Kowalski <malind.nikulski@gmail.com>
2019-08-06 13:44:58 -0600

    add note of what i want App to look like eventually

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Beers from './Beers';
+// import BeerDetail from '../components/BeerDetail';
 
 class App extends React.Component {
   state = {beers: []}
@@ -7,10 +8,26 @@ class App extends React.Component {
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
-       <Beers beers={this.state.beers}/>
+        <Beers beers={this.state.beers}/>
       </div>
     )
   }
 }
 
 export default App
+
+
+//would eventually like to use this format
+//left side view with list of beers, select a beer and beer details show on right side
+
+// <div className='ui container grid' style={{ marginTop: '15px' }}>
+//   <div className='ui row'>
+//     <div className='column eight wide'>
+//       <Beers beers={this.state.beers} />
+//     </div>
+//
+//     <div className='column eight wide'>
+//       <BeerDetail />
+//     </div>
+//   </div>
+// </div>
